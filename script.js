5c6055d44361446495f55a715dc005ac31a2b1ab
ezequieli12 <48675678@est.ort.edu.ar>
2025-03-13 12:06:43 -0300

    push1

---
+++ script.js
@@ -0,0 +1,23 @@
+function ErrorAlValidarContraseña() 
+{
+    if (Contraseña.length<8)
+    {
+        console.log("Tu contraseña no tiene los suficientes caracteres")
+    }
+    else if (Contraseña.includes())
+}
+function ErrorAlValidarNombre() 
+{
+    if (Nombre.length<3)
+    {
+        console.log("Tu contraseña no tiene los suficientes caracteres")
+    }
+    
+}
+function ValidacionDe2Pasos()
+{
+    if ()
+    {
+
+    }
+}
